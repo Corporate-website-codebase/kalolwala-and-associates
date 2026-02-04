4f0dba9fc774a3ef23f364dd52e84e2560928fc8
tltlabs <digital@thelemontheory.com>
2026-02-04 22:29:46 +0530

    add organization & local business schema

--- src/app/offerings/page.tsx
+++ src/app/offerings/page.tsx
@@ -11,11 +11,81 @@ export const metadata: Metadata = {
   },
 };
 
+const serviceSchema = {
+  "@context": "https://schema.org",
+  "@type": "ItemList",
+  "@id": "https://www.kalolwala.com/offerings/#services",
+  "name": "Service Offerings by Kalolwala and Associates",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "item": {
+        "@type": "Service",
+        "@id": "https://www.kalolwala.com/offerings/#annual-reporting",
+        "name": "Annual and Integrated Reporting",
+        "provider": {
+          "@id": "https://kalolwala.com/#organization"
+        }
+      }
+    },
+    {
+      "@type": "ListItem",
+      "position": 2,
+      "item": {
+        "@type": "Service",
+        "@id": "https://www.kalolwala.com/offerings/#esg-reporting",
+        "name": "Sustainability and ESG Reporting",
+        "provider": {
+          "@id": "https://kalolwala.com/#organization"
+        }
+      }
+    },
+    {
+      "@type": "ListItem",
+      "position": 3,
+      "item": {
+        "@type": "Service",
+        "@id": "https://www.kalolwala.com/offerings/#corporate-presentations",
+        "name": "Corporate Presentations and Communication Collaterals",
+        "provider": {
+          "@id": "https://kalolwala.com/#organization"
+        }
+      }
+    },
+    {
+      "@type": "ListItem",
+      "position": 4,
+      "item": {
+        "@type": "Service",
+        "name": "Branding and Design Services",
+        "provider": {
+          "@id": "https://kalolwala.com/#organization"
+        }
+      }
+    },
+    {
+      "@type": "ListItem",
+      "position": 5,
+      "item": {
+        "@type": "Service",
+        "name": "Digital Communication Solutions",
+        "provider": {
+          "@id": "https://kalolwala.com/#organization"
+        }
+      }
+    }
+  ]
+};
 
 const Services = () => {
 
     return (
         <div>
+            <script
+              type="application/ld+json"
+              dangerouslySetInnerHTML={{ __html: JSON.stringify(serviceSchema) }}
+            />
             <Header />
             <HorizontalScroll />
             <ImpactSection />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,79 @@ import StatsTop from "@/Home/StatsTop";
 import Portfolio from "@/Home/Portfolio";
 import Footers from "@/components/Footers";
 import Stats from "@/Home/Stats";
-// import Founders from "@/Home/Founders";
+
+const organizationSchema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "@id": "https://kalolwala.com/#organization",
+  "name": "Kalolwala and Associates",
+  "url": "https://kalolwala.com",
+  "logo": "https://k-and-a-assets.s3.ap-south-1.amazonaws.com/kna-logo.jpeg",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+91-33-4007-7794",
+    "contactType": "customer service"
+  },
+  "sameAs": [
+    "https://in.linkedin.com/company/kalolwala-associates-private-limited",
+    "https://www.instagram.com/kalolwalaassociates/?hl=en",
+    "https://www.facebook.com/kalolwalaassociates/",
+    "https://x.com/KalolwalaAssoc"
+  ]
+};
+
+const localBusinessSchema = {
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "@id": "https://kalolwala.com/#localbusiness-kolkata",
+  "name": "Kalolwala and Associates - Kolkata",
+  "url": "https://kalolwala.com",
+  "parentOrganization": {
+    "@id": "https://kalolwala.com/#organization"
+  },
+  "telephone": "+91-33-4007-7794",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "33K, Tiljala",
+    "addressLocality": "Kolkata",
+    "addressRegion": "West Bengal",
+    "postalCode": "700039",
+    "addressCountry": "IN"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 22.52858688286508,
+    "longitude": 88.37952498368794
+  },
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ],
+      "opens": "10:00",
+      "closes": "18:00"
+    }
+  ],
+  "hasMap": "https://www.google.com/maps?q=22.52858688286508,88.37952498368794"
+};
 
 export default function Home() {
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
+      />
       <Hero />
       <Discover />
       <Locations />
