239c964d82e8c53a3708bd2bf1edccca6bc99c93
tltlabs <digital@thelemontheory.com>
2026-02-05 21:35:52 +0530

    add GTM code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 // import { Dosis } from "next/font/google";
 import { Anton } from "next/font/google";
@@ -30,7 +31,7 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/",
     languages: {
-      "en": "https://kalolwala.com/",
+      en: "https://kalolwala.com/",
       "x-default": "https://kalolwala.com/",
     },
   },
@@ -44,17 +45,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="nZdF0YGHOkhdaZjvtTM7t5y7tvx23ggkUuKt3HwUopM" />
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N6SR3K3C');`,
+          }}
+        />
+        <meta
+          name="google-site-verification"
+          content="nZdF0YGHOkhdaZjvtTM7t5y7tvx23ggkUuKt3HwUopM"
+        />
         <link rel="stylesheet" href="https://use.typekit.net/zmg6oqe.css" />
       </head>
       <body className={`${anton.variable} ${noto.variable}  antialiased`}>
-      <PassTransitionProvider colors={[ "#555555","#3D3D3D" ,"#252525"]}>
-        <Navbar />
-        <SmoothScroll>
-          <main className="relative w-full h-full">
-            {children}
-          </main>
-        </SmoothScroll>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N6SR3K3C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <PassTransitionProvider colors={["#555555", "#3D3D3D", "#252525"]}>
+          <Navbar />
+          <SmoothScroll>
+            <main className="relative w-full h-full">{children}</main>
+          </SmoothScroll>
         </PassTransitionProvider>
       </body>
     </html>
